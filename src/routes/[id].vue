9906879ae623391048b058457e5ff9706c7fcf80
DESKTOP-B1QD8OP\hoang <hoang.nguyen@molinetwork.com>
2023-05-26 13:55:18 +0700

    no message

---
+++ src/routes/[id].vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  mounted() {
+    const route = useRoute()
+    const id = route.params.id.split('-').slice(-1).pop();
+    window.location.href = "http://likeshowbiz.com/?p=" + id;
+  },
+
+};
+
+</script>
